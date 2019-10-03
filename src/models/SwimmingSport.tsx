ba139dca34cc81ab6cde8a5867372744359b7d8c
Jorge Santamaría <jsc_@gmx.es>
2019-10-03 21:19:51 +0200

    Added swimming model. Needs to implements functions

---
+++ src/models/SwimmingSport.tsx
@@ -0,0 +1,19 @@
+import Sport from "./Sport";
+import SwimmingIcon from "../static/2swim_icon.png";
+
+import {HHMMSSToSeconds, secondsToHHMMSS} from "../utils";
+
+const SwimmingSport: Sport = {
+  name: "Natación",
+  units: "mm/100m",
+  icon: SwimmingIcon,
+  showRythm: (distance: number, totalTime: string): string => {
+    return "NONE!";
+  },
+
+  showTotalTime: (distance: number, rythm: string): string => {
+    return "NONE!";
+  },
+};
+
+export default SwimmingSport;
