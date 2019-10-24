16443568831672e4090adf9e7ab4572fa3e2dc77
Jorge Santamaría <jsc_@gmx.es>
2019-10-24 17:15:51 +0200

    Added ResetButton

---
+++ src/components/LapTimesDisplay/ResetButton.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+
+import ResetIcon from "../CloseIcon";
+
+interface IResetButtonProps {
+  children: string;
+}
+
+const StyledResetButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  border: none;
+  background-color: #dfdfdf;
+  border-color: blue;
+  padding: 10px 32px 10px 32px;
+  border-radius: 8px;
+  cursor: pointer;
+  color: #333
+  font-weight: bold;
+
+  transition: all 150ms ease;
+
+  :hover {
+    box-shadow: 0px 3px 7px rgba(1, 0, 0, 0.25),
+      0px 5px 5px rgba(1, 0, 0, 0.22);
+  }
+
+  :active {
+    transition: box-shadow 0.01s ease-out;
+    box-shadow: none;
+  }
+`;
+
+const StyledIcon = styled.div`
+  margin-right: 8px;
+  width: 12px;
+  height: 12px;
+
+  svg {
+    fill: #333;
+  }
+`;
+
+const ResetButton = ({children}: IResetButtonProps): React.ReactElement => {
+  return (
+    <StyledResetButton>
+      <StyledIcon>
+        <ResetIcon />
+      </StyledIcon>
+      {children}
+    </StyledResetButton>
+  );
+};
+
+export default ResetButton;
