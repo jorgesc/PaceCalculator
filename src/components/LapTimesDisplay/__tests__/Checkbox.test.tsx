912765fa8b3ae57f348702dffcf43d49104475c4
Jorge Santamaría <jsc_@gmx.es>
2019-10-30 21:38:48 +0100

    Fixed small bug in checkbox tests

--- src/components/LapTimesDisplay/__tests__/Checkbox.test.tsx
+++ src/components/LapTimesDisplay/__tests__/Checkbox.test.tsx
@@ -3,15 +3,15 @@ import thunk from "redux-thunk";
 import Enzyme, {shallow, mount, ReactWrapper} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-import Checkbox, {StyledCheckboxContainer} from "../Checkbox";
+import Checkbox from "../../Checkbox/Checkbox";
 
 Enzyme.configure({adapter: new Adapter()});
 
 describe("Checkbox tests", () => {
   it("Calls the callback function when clicked", () => {
     const onChange = jest.fn();
-    const wrapper = mount(<Checkbox onChange={onChange} checked={false} />);
-    wrapper.find(StyledCheckboxContainer).simulate("click");
+    const wrapper = mount(<Checkbox onChange={onChange} value={false} />);
+    wrapper.simulate("click");
     expect(onChange.mock.calls).toHaveLength(1);
   });
 });
